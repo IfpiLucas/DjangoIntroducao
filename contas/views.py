3b39839ddf57594dad316dcb2f8813a7378f4b61
IfpiLucas <ifpilucas@gmail.com>
2018-12-24 09:20:12 -0300

    Fazendo listagem com Models

--- contas/views.py
+++ contas/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Transacao
 import datetime
 # Create your views here.
 
 def home(request):
-    now = datetime.datetime.now()
-    return render(request, 'contas/home.html')
+    data = {}
+    data['now'] = datetime.datetime.now()
+    data['transacoes'] = ['t1', 't2', 't3']
+    return render(request, 'contas/home.html', data)
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+    return render(request, 'contas/listagem.html', data)
+
+
+
+
+
+
+
+
+
+
